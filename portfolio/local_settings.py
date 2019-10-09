760350fd99c5096dd0c4f45f1d04c279070fa246
Anorax <anorax.isa@gmail.com>
2019-10-09 21:13:14 +0200

    local settings updated

--- portfolio/local_settings.py
+++ portfolio/local_settings.py
@@ -1,10 +1,10 @@
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bzx+b@*7_2c5ew58%-8(3qjhgx=5+y7v472u$dk#3yq&*71r37'
+# SECRET_KEY = 'bzx+b@*7_2c5ew58%-8(3qjhgx=5+y7v472u$dk#3yq&*71r37'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
@@ -17,4 +17,4 @@ DATABASES = {
     }
 }
 
-DARKSKY_API_KEY = '89aeb1016e0e5068c14f91c54687d28c'
+# DARKSKY_API_KEY = '89aeb1016e0e5068c14f91c54687d28c'
