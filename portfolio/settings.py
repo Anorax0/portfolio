b1708e3bc9c065247e9c20a384e932775f57ddda
Anorax <anorax.isa@gmail.com>
2019-10-09 21:10:29 +0200

    djange heroku added

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -146,3 +147,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 DARKSKY_API_KEY = os.environ.get('DARKSKY_API_KEY_VALUE')
 
 # from .local_settings import *
+
+django_heroku.settings(locals())
