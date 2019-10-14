cfeeeb8701f4633a487a0822b83b69bca8fb2a09
Anorax <anorax.isa@gmail.com>
2019-10-14 22:13:40 +0200

    still working with s3 bucket

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -177,5 +177,8 @@ AWS_S3_REGION_NAME = 'eu-west-3'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_QUERYSTRING_AUTH = False
+
 
 # from .local_settings import *
