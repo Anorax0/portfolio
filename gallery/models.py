eeeb767e29b13a3fc54f06409ab423088461e906
Anorax <anorax.isa@gmail.com>
2019-10-14 22:06:29 +0200

    still working with s3

--- gallery/models.py
+++ gallery/models.py
@@ -4,7 +4,7 @@ from django import forms
 
 
 class GalleryItems(models.Model):
-    image = models.ImageField(upload_to='images/meme')
+    image = models.ImageField(upload_to='images/gallery_photos')
     description = models.CharField(max_length=300)
     title = models.CharField(max_length=100, default='Image')
     date = models.DateTimeField(default=datetime.now, blank=True)
---
+++ portfolio/local_settings.py
@@ -0,0 +1,20 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'bzx+b@*7_2c5ew58%-8(3qjhgx=5+y7v472u$dk#3yq&*71r37'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
+
+# ALLOWED_HOSTS = []
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'portfolio',
+#         'USER': 'postgres',
+#         'PASSWORD': 'justword',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
+ALLOWED_HOSTS = ['anorax.herokuapp.com']
+# DARKSKY_API_KEY = '89aeb1016e0e5068c14f91c54687d28c'
