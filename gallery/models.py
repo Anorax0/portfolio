0bbbf9f995c311fb68b422814ce3988a54f4bb43
Anorax <anorax.isa@gmail.com>
2019-10-14 22:18:15 +0200

    still working with s3

--- gallery/models.py
+++ gallery/models.py
@@ -4,7 +4,7 @@ from django import forms
 
 
 class GalleryItems(models.Model):
-    image = models.ImageField(upload_to='images/gallery_photos')
+    image = models.ImageField(upload_to='photos')
     description = models.CharField(max_length=300)
     title = models.CharField(max_length=100, default='Image')
     date = models.DateTimeField(default=datetime.now, blank=True)
