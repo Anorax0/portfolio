22bb9d7442bf77356142cff0a4f020e5ffbd75e3
Anorax <anorax.isa@gmail.com>
2019-10-15 22:49:44 +0200

    weather API building

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, default='')
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
