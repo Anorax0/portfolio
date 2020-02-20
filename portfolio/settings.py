d02d64ae0b548244db45e1a2b3c7ad25a0438b60
Anorax <anorax.isa@gmail.com>
2020-02-20 21:39:04 +0100

    Refactoring static files for deployment

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -121,7 +121,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'portfolio/static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 
 
 # Media Folder Settings
